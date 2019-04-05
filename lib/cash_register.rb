6497d540e4161f435ca6d561c969ba2582602b0f
Susan Tan <ssusantan9@gmail.com>
2019-04-05 04:00:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,11 +20,23 @@ def items
    items_total = []
    @items.map do |product|
      for num in 1..product[:quantity] 
-     items_total << product[:name]
+     items_total << product[:title]
    end 
  end
  items_total
   end  
+  
+  
+    # def items
+    # item_names = []
+    # @cart.each do | item_info |
+    #   #test expects product name * quantity...
+    #   for qty in 1..item_info[:quantity] 
+    #     item_names << item_info[:name]
+    #   end 
+    # end 
+    # item_names
+
   
   # def apply_discount
   # end 
